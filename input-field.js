a76f88f77a787b6b8191a640fdeaae193448a1cb
sakil-ahmed <sakilahmed.dev@gmail.com>
2024-03-02 08:21:25 +0600

    fix css

--- input-field.js
+++ input-field.js
@@ -7519,7 +7519,6 @@ $(document).ready(async function () {
 
 $(document).ready(async function () {
 
-
     const customStyle = `
     .dropzone{
     background-color:transparent !important;
@@ -7537,6 +7536,16 @@ $(document).ready(async function () {
         .dz-message-link{
             text-decoration: underline;
         }
+        
+           .dropzone .dz-preview.dz-image-preview{
+            background-color: transparent;
+        }
+
+        .dropzone .dz-preview .dz-remove{
+            color: #000000;
+            text-decoration: none;
+        }
+        
     `
 
     const addDropzoneCss = async () => {
@@ -7570,14 +7579,15 @@ $(document).ready(async function () {
 
     const dropzoneList = $('.dropzone')
 
-    const valuesArray = Object.values(dropzoneList)
+    const dropzoneIds = Object.values(dropzoneList)
         .map((value) => value.id);
 
-    valuesArray.map(async function (i) {
+    dropzoneIds.map(async function (i) {
 
         if (i !== undefined) {
 
             const element = document.getElementById(i)
+            const $element = $(`#${i}`)
 
             const attrs = element.getAttributeNames().reduce((acc, name) => {
                 return {...acc, [name.replace(/-/g, "_")]: element.getAttribute(name)};
@@ -7585,7 +7595,7 @@ $(document).ready(async function () {
 
             const dropzone = new Dropzone(`#${i}`,
                 {
-                    url: "#",// "https://sakil-ahmed.s3.amazonaws.com/",
+                    url: "#",
                     method: "post",
                     paramName: "file",
                     autoProcessQueue: true,
@@ -7599,17 +7609,15 @@ $(document).ready(async function () {
             dropzone.on('addedfile', function (file) {
             })
             dropzone.on('success', function (file) {
-                console.log(file)
+                const borderRadius = $element.css('border-radius')
+                $element.find('.dz-image').css('border-radius',borderRadius || 0)
             })
             dropzone.on('removedfile', function (file) {
-                console.log(file)
             })
 
             await $('.dz-message').each(function () {
                 $(this).html(`<p class="dz-message-content"><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 1024 1024" height="1.5em" width="1.5em" xmlns="http://www.w3.org/2000/svg"><path d="M518.3 459a8 8 0 0 0-12.6 0l-112 141.7a7.98 7.98 0 0 0 6.3 12.9h73.9V856c0 4.4 3.6 8 8 8h60c4.4 0 8-3.6 8-8V613.7H624c6.7 0 10.4-7.7 6.3-12.9L518.3 459z"></path><path d="M811.4 366.7C765.6 245.9 648.9 160 512.2 160S258.8 245.8 213 366.6C127.3 389.1 64 467.2 64 560c0 110.5 89.5 200 199.9 200H304c4.4 0 8-3.6 8-8v-60c0-4.4-3.6-8-8-8h-40.1c-33.7 0-65.4-13.4-89-37.7-23.5-24.2-36-56.8-34.9-90.6.9-26.4 9.9-51.2 26.2-72.1 16.7-21.3 40.1-36.8 66.1-43.7l37.9-9.9 13.9-36.6c8.6-22.8 20.6-44.1 35.7-63.4a245.6 245.6 0 0 1 52.4-49.9c41.1-28.9 89.5-44.2 140-44.2s98.9 15.3 140 44.2c19.9 14 37.5 30.8 52.4 49.9 15.1 19.3 27.1 40.7 35.7 63.4l13.8 36.5 37.8 10C846.1 454.5 884 503.8 884 560c0 33.1-12.9 64.3-36.3 87.7a123.07 123.07 0 0 1-87.6 36.3H720c-4.4 0-8 3.6-8 8v60c0 4.4 3.6 8 8 8h40.1C870.5 760 960 670.5 960 560c0-92.7-63.1-170.7-148.6-193.3z"></path></svg>  Drag and Drop or <span class="dz-message-link">Browse file</span> </p>`)
             })
-
-            let $element = $(`#${i}`)
 
             $element.find('.dz-message-content svg, .dz-message-link')
                 .css('color', attrs.data_default_color);
