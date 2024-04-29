18c2aca0ce10eaf9685c6c1dcbf0fef995e34ce4
sakil-ahmed <sakilahmed.dev@gmail.com>
2024-04-29 15:48:16 +0600

    update

--- input-field.js
+++ input-field.js
@@ -446,6 +446,8 @@ $(document).ready(async function () {
 
     const input = document.querySelector("#phone");
     window.intlTelInput(input, {
+        initialCountry: "us",
+        hiddenInput: () => ({country: "country_code"}),
         utilsScript: "https://cdn.jsdelivr.net/npm/intl-tel-input@21.2.7/build/js/utils.js",
         countrySearch: false,
 
