160144645dc28c58214b8f86da803fee0ee914fe
sakil-ahmed <sakilahmed.dev@gmail.com>
2024-04-29 16:32:24 +0600

    update

--- input-field.js
+++ input-field.js
@@ -448,7 +448,7 @@ $(document).ready(async function () {
     window.intlTelInput(input, {
         initialCountry: "auto",
         geoIpLookup: callback => {
-            fetch("https://ipapi.co/json")
+            fetch("https://ipapi.co/json/",)
                 .then(res => res.json())
                 .then(data => callback(data.country_code))
                 .catch(() => callback("us"));
